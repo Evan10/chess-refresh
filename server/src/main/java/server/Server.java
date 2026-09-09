fa3abbbc6873279b61cb769d8a81deee0e6782f3
Evan <evanroyal1@gmail.com>
2026-09-08 18:52:57 -0600

    setup api endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -7,10 +7,18 @@ public class Server {
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
-        // Register your endpoints and exception handlers here.
-
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .before((context )->{})
+                .post("/user",(context)->{})
+                .post("/session",(context)->{})
+                .delete("/session",(context) -> {})
+                .get("/game",(context) -> {})
+                .post("game",(context)->{})
+                .put("/game",(context)->{})
+                .delete("/db",(context)->{})
+                .error(400,context->{})
+                .error(401,(context)->{})
+                .error(500,(context)->{});
     }
 
     public int run(int desiredPort) {

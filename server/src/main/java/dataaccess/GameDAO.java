7d5f034e398c4ab7fb60945b2f59dccd742235a9
Evan <evanroyal1@gmail.com>
2026-09-10 18:14:41 -0600

    remove unused files ChessUserRole and InproperPermissionException

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -2,7 +2,6 @@ package dataaccess;
 
 import chess.ChessGame;
 import model.GameData;
-import service.ChessUserRole;
 
 import java.util.Collection;
 
--- server/src/main/java/service/InproperPermissionException.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-public class InproperPermissionException extends RuntimeException {
-    public InproperPermissionException(String message) {
-        super(message);
-    }
-}
